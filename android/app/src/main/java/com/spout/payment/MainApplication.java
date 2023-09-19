a17af3ad41820b64e6c3e4c67c911f1a866b4f89
macbook <macbook@macbooks-MacBook-Pro.local>
2023-09-19 06:20:06 -0700

    name changed

--- android/app/src/main/java/com/spout/payment/MainApplication.java
+++ android/app/src/main/java/com/spout/payment/MainApplication.java
@@ -1,4 +1,4 @@
-package com.spoutmobile;
+package com.spout.payment;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
--- ios/SpoutPayTests/SpoutPayTests.m
+++ ios/SpoutPayTests/SpoutPayTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface spoutmobileTests : XCTestCase
+@interface SpoutPayTests : XCTestCase
 
 @end
 
-@implementation spoutmobileTests
+@implementation SpoutPayTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
