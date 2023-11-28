e6116e46732cdb7d2098bed1b49cdb6f2382a4a1
macbook <macbook@macbooks-MacBook-Pro.local>
2023-11-28 11:55:00 -0800

    changes to app name and version

--- ios/payscholarTests/payscholarTests.m
+++ ios/payscholarTests/payscholarTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface FixItEasyTests : XCTestCase
+@interface payscholarTests : XCTestCase
 
 @end
 
-@implementation FixItEasyTests
+@implementation payscholarTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
