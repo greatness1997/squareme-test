e555b37571b6caeb120f16cc55e883fb6b764a62
macbook <macbook@macbooks-MacBook-Pro.local>
2023-11-28 14:10:15 -0800

    ne chnages

--- ios/payscholarTests/payscholarTests.m
+++ ios/payscholarTests/payscholarTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface payscholarTests : XCTestCase
+@interface PayScholarTests : XCTestCase
 
 @end
 
-@implementation payscholarTests
+@implementation PayScholarTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
